7b532f6fadc7b03a65d0ae5ec4ed7e9b98baf3f1
Drew McClaren <d.mcclellan013@gmail.com>
2026-01-02 19:06:57 -0500

    Create run_script_from_github.sh
    Add a script to run another script directly from GitHub.

---
+++ Scripts/github/run_script_from_github.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vars
+# github url
+url="https://raw.githubusercontent.com/you/repo/main/script.sh"
+# just the script name from the url.
+script_name="$(basename "$url")"
+## script start ##
+echo "running script_name...please wait..."
+# run the script without saving it to disk.
+curl -fsSL "$url" | sudo bash
+## script end ##
+
