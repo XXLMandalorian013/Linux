7ade1f14ee7867c7fa721dc723f8cd81e08d2c62
Drew McClellan <d.mcclellan013@gmail.com>
2026-01-29 20:06:19 -0500

    docker install script done

--- Scripts/Software/Installers/docker/64_bit/install_docker_64bit.sh
+++ Scripts/Software/Installers/docker/64_bit/install_docker_64bit.sh
@@ -3,59 +3,33 @@
 ## script start
 # vars
 # folder location.
-# software service name
-servicename="sophos-spl\.service"
-folder="var"
-# tmp folder name
-tmpfolder="tmp-99"
-# URL of the installer
-url="https://dzr-api-amzn-us-west-2-fa88.api-upe.p.hmr.sophos.com/api/download/12345678901234567890/SophosSetup.sh"
-# function to create a tmp folder, if it doesn't exist.
-# output file name
-outfile="SophosSetup.sh"
-# checks to see if sophos is already installed, if so, ends the script.
-pre-installcheck() {
-    if systemctl list-units --type=service --all | grep -q "^$servicename"; then
-    echo "sophos-spl.service found...exiting script..."
-    exit 0
+# software name.
+software_name="docker"
+# url of the installer.
+url="https://get.docker.com"
+# checking for docker service status
+if systemctl is-active --quiet "$software_name"; then
+    echo "$software_name is already installed...ending script..."
+else
+    echo "$software_name is not installed...installing $software_name..."
+    # updating package index
+    echo "updating package index...please wait..."
+    sudo apt-get update
+    # downloading the installer script.
+    if curl -s -o /dev/null -w "%{http_code}" "$url" | grep -q '^200$'; then
+        echo "URL is reachable and returned 200 OK..downloading installer..."
+        curl -fsSL $url -o get-docker.sh
+        # running the downloaded installer script.
+        echo "running $software_name installer..."
+        sudo sh get-docker.sh
+        if systemctl is-active --quiet $software_name; then
+            echo "the docker service is running...ending script..."
+        else
+            echo "the docker service is not running"
+        fi
     else
-    echo "s$servicename not found...continuing script..."
-fi
-}
-create_folder() {
-    local tmpfolder="$1"
-    if [ -d "$tmpfolder" ]; then
-        echo "Folder '$tmpfolder' already exists."
-    else
-        echo "Folder '$tmpfolder' not found. Creating it now..."
-        sudo mkdir "$tmpfolder"
+    echo "URL is not reachable or returned non-200"
     fi
-}
-# function to download the Sophos installer.
-download_installer() {
-    # download using curl
-    curl -L -o "$folder/$tmpfolder/$outfile" "$url"
-    echo "downloaded $outfile to $folder/$tmpfolder."
-}
-# setting folder location for the DL and making the script executable.
-make_executable() {
-    cd "/$folder/$tmpfolder"
-    chmod +x "$outfile"
-}
-run_installer() {
-    ./"$outfile" --products=antivirus,mdr,xdr
-}
-## functions.
-# checks to see if sophos is already installed, if so, ends the script.
-pre-installcheck
-# function to create a tmp folder, if it doesn't exist. Specified the var for the required folder.
-create_folder "/$folder/$tmpfolder"
-# function to download the Sophos installer.
-download_installer
-# setting folder location for the DL and making the script executable.
-make_executable
-# running the installer with required products.
-run_installer
+fi
 ## script end
-
 
